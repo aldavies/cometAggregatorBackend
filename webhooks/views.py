1438f5f2f210a3381d58c1386ece9e0d110d8bc2
aldavies <aldavies@umich.edu>
2020-08-24 01:48:15 -0400

    first commit

---
+++ webhooks/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt 
+@require_POST
+def example(request):
+	return HttpResponse('Hello, world. This is the webhook response.')
